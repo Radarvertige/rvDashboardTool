cd1a493d89306d0b4c443f6296c5ba88833a3fec
Daniel <d.vanderstam@radarvertige.nl>
2024-08-26 10:53:20 +0200

    groep url werkt en dashboardjs aangepast met tokens

--- src/utils/url.js
+++ src/utils/url.js
@@ -3,9 +3,9 @@ import { isDebugMode } from './keyboard';
 const YOURLS_API_URL = 'https://rdar.nl/yourls-api.php';
 const YOURLS_SIGNATURE = '157448975e';
 
-export const shortenUrl = async (finalUrl, customSlug) => {
+export const shortenUrl = async (finalUrl, groupSlug) => {
   try {
-    const requestUrl = `${YOURLS_API_URL}?signature=${YOURLS_SIGNATURE}&action=shorturl&format=json&url=${encodeURIComponent(finalUrl)}&keyword=${encodeURIComponent(customSlug)}`;
+    const requestUrl = `${YOURLS_API_URL}?signature=${YOURLS_SIGNATURE}&action=shorturl&format=json&url=${encodeURIComponent(finalUrl)}&keyword=${encodeURIComponent(groupSlug)}`;
 
     if (isDebugMode()) {
       console.log(`Request URL: ${requestUrl}`);
@@ -34,22 +34,16 @@ export const generateShortUrl = async (token, dashboard, group) => {
     const dashboardUrl = dashboard.url;
     const combinedUrl = `${dashboardUrl}${token}`;
 
-    // Construct the custom keyword by combining team, dashboard name, and group, using hyphens
-    const teamSlug = dashboard.team.replace(/\s+/g, '-').toLowerCase();
-    const dashboardNameSlug = dashboard.name.replace(/\s+/g, '-').toLowerCase();
+    // Construct the keyword slug by only using the group name
     const groupSlug = group ? group.replace(/\s+/g, '-').toLowerCase() : '';
 
-    const customSlug = groupSlug 
-      ? `${teamSlug}-${dashboardNameSlug}-${groupSlug}`
-      : `${teamSlug}-${dashboardNameSlug}`;
-
     if (isDebugMode()) {
-      console.log(`Custom Keyword (Slug): ${customSlug}`);
+      console.log(`Group Slug (Keyword): ${groupSlug}`);
       console.log(`Combined URL before shortening: ${combinedUrl}`);
     }
 
-    // Shorten the combined URL using the custom slug
-    return await shortenUrl(combinedUrl, customSlug);
+    // Shorten the combined URL using the group slug
+    return await shortenUrl(combinedUrl, groupSlug);
 
   } catch (error) {
     console.error(`Error generating short URL:`, error);
